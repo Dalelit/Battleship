e594954364be8682359c20aee5ab06c30899c4e1
Dalelit <dale.coding@gmail.com>
2018-04-05 23:33:51 +1000

    Added the board and UI stuff

--- battleship.js
+++ battleship.js
@@ -1,31 +1,92 @@
 $(document).ready(function()
 {
-    function logInfo(msg)
+    /*
+     * To Do - tidy ups
+     * - board size doesn't use the returned game info... still hard coded to 10
+     * - not responsive
+     * - consider the aesthetics...
+     */
+
+    boardWidth = 200;
+    boardHeight = boardWidth;
+    cellSize = boardWidth / 10;
+    radius = 0.4 * cellSize;
+    playerCanvas = document.getElementById("playerBoard");
+    opponentCanvas = document.getElementById("opponentBoard");
+
+    function logInfo(msg) // to do - probably a better way
     {
         $("#logDiv").html(msg + "<br>" + $("#logDiv").html());
     }
 
+    function drawCell(context, row, col, color)
+    {
+        context.beginPath();
+        context.arc(col * cellSize + cellSize/2, row * cellSize + cellSize/2, radius, 0, 2*Math.PI);
+        context.fillStyle = color;
+        context.fill();
+    }
+
+    function drawBoard(canvas, columns, rows, color)
+    {
+        let ctx = canvas.getContext("2d");
+
+        for (let r = 0; r < rows; r++)
+        {
+            for (let c = 0; c < columns; c++) drawCell(ctx, r, c, color);
+        }
+    }
+
+    function drawBoat(canvas, c1, r1, c2, r2)
+    {
+        let ctx = canvas.getContext("2d");
+        let fillColor = "Grey";
+
+        if (r1 == r2) //  horizontal boat
+        {
+            for (let i = c1; i <= c2; i++) drawCell(ctx, r1, i, fillColor);
+        }
+        else //  vertical boat
+        {
+            for (let i = r1; i <= r2; i++) drawCell(ctx, i, c1, fillColor);
+        }
+    }
+
     function createPlayerBoard(gameInfo)
     {
-        let $table = $('<table/>').attr('id','playertable');
+        drawBoard(playerCanvas, gameInfo.columns, gameInfo.rows, "SkyBlue");
 
-        for (let r =0; r < gameInfo.rows; r++)
+        for (let i = 0; i < gameInfo.boats.length; i++)
         {
-            let $row = $('<tr/>').attr('id', 'row'+r);
-            for (let c =0; c < gameInfo.columns; c++)
-            {
-                let $col = $('<td>0</td>').attr('id', 'pcell'+r+c);
-                $col.click(function() { playerBoardCellClicked(c,r); });
-                $row.append($col);
-            }
-            $table.append($row);
+            let boat = gameInfo.boats[i];
+            drawBoat(playerCanvas, boat['start'][0], boat['start'][1], boat['end'][0], boat['end'][1]);
         }
-        $('#playerBoardDiv').html($table);
+    }
+
+    function playerBoardClicked(event)
+    {
+        // playerBoardCellClicked(Math.floor(event.offsetX / cellSize), Math.floor(event.offsetY / cellSize));
     }
 
     function playerBoardCellClicked(col, row)
     {
         logInfo("Player board cell click " + col + "," + row);
+    }
+
+    function createOpponentBoard(gameInfo)
+    {
+        drawBoard(opponentCanvas, gameInfo.columns, gameInfo.rows, "LightSkyBlue");
+    }
+
+    function opponentBoardClicked(event)
+    {
+        opponentBoardCellClicked(Math.floor(event.offsetX / cellSize), Math.floor(event.offsetY / cellSize));
+    }
+
+    function opponentBoardCellClicked(col, row)
+    {
+        // logInfo("Opponent board cell click " + col + "," + row);
+        drawCell(opponentCanvas.getContext("2d"), row, col, "Green");
     }
 
     function gameCreated(data, status)
@@ -34,12 +95,13 @@ $(document).ready(function()
         logInfo(data);
         var gameInfo = JSON.parse(data);
         createPlayerBoard(gameInfo);
+        createOpponentBoard(gameInfo);
         logGameInfo(gameInfo);
     }
 
     function createGame()
     {
-        var createGameUrl = $("#serverUrl").val() + "newgame?gameId=" + encodeURIComponent($("#gameId").val()) + "&player=" + encodeURIComponent($("#playerName").val());
+        let createGameUrl = $("#serverUrl").val() + "newgame?gameId=" + encodeURIComponent($("#gameId").val()) + "&player=" + encodeURIComponent($("#playerName").val());
         $.get(createGameUrl, gameCreated );
     }
     
@@ -47,13 +109,13 @@ $(document).ready(function()
     {
         logInfo(status);
         logInfo(data);
-        var gameInfo = JSON.parse(data);
+        let gameInfo = JSON.parse(data);
         logGameInfo(gameInfo);
     }
 
     function joinGame()
     {
-        var joinGameUrl = $("#serverUrl").val() + "games/" + encodeURIComponent($("#gameId").val()) + "/join?player=" + encodeURIComponent($("#playerName").val());
+        let joinGameUrl = $("#serverUrl").val() + "games/" + encodeURIComponent($("#gameId").val()) + "/join?player=" + encodeURIComponent($("#playerName").val());
         $.get(joinGameUrl, gameJoined );
     }
 
@@ -64,14 +126,14 @@ $(document).ready(function()
 
     function logGameInfo(game)
     {
-        info  = "Game id: " + game.id;
+        let info = "Game id: " + game.id;
         info += "<br>Player 1: " + game.player1;
         info += "<br>Player 2: " + game.player2;
 
         info += "<br>Boats: " + game.boats.length;
-        for (i = 0; i < game.boats.length; i++)
+        for (let i = 0; i < game.boats.length; i++)
         {
-            var boat = game.boats[i];
+            let boat = game.boats[i];
             info += "<br>" + boat['start'] + '-->' + boat['end'] + " length " + boat['length'];
         }
 
@@ -83,5 +145,6 @@ $(document).ready(function()
 	$("#createBtn").on('click', createGame);
     $("#clearBtn").on('click', function() {$("#logDiv").html("");});
     $("#logGamesBtn").on('click', logAvailableGames);
-	// $("#inputURL").on('change', testIt);
+    $("#playerBoard").on('click', playerBoardClicked);
+    $("#opponentBoard").on('click', opponentBoardClicked);
 });
